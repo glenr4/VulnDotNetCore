d20a30ae083717e46978581ca3f138d001275296
Glen Rutherford <glenrutherford4@gmail.com>
2020-01-19 10:36:09 +1100

    Re-enable Https redirection so that issuers match

--- VulnDotNetCore/Startup.cs
+++ VulnDotNetCore/Startup.cs
@@ -82,11 +82,9 @@ namespace VulnDotNetCore
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-
-                app.UseHttpsRedirection();
             }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
