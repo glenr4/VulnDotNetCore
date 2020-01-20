b57ef799f096d11f24c7ee1bdd6b2f7f0ed3b7c2
Glen Rutherford <glenrutherford4@gmail.com>
2020-01-20 15:16:24 +1100

    Added migration for Test table

---
+++ VulnDotNetCore/Data/Migrations/20200120013301_Added Test table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VulnDotNetCore.Data.Migrations
+{
+    public partial class AddedTesttable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Test",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Test", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Test");
+        }
+    }
+}
