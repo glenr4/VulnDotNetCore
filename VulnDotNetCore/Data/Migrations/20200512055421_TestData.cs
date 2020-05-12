2a59c06cb08f687ca1161a1f6b059a9f08052dae
Glen Rutherford <glenrutherford4@gmail.com>
2020-05-12 16:57:39 +1000

    Seed test data

---
+++ VulnDotNetCore/Data/Migrations/20200512055421_TestData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+namespace VulnDotNetCore.Data.Migrations
+{
+    public partial class TestData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Test", new[] { "ID", "Description" }, new object[] { Guid.NewGuid(), "Test description 1" });
+            migrationBuilder.InsertData("Test", new[] { "ID", "Description" }, new object[] { Guid.NewGuid(), "Test description 2" });
+            migrationBuilder.InsertData("Test", new[] { "ID", "Description" }, new object[] { Guid.NewGuid(), "Test description 3" });
+            migrationBuilder.InsertData("Test", new[] { "ID", "Description" }, new object[] { Guid.NewGuid(), "Test description 4" });
+            migrationBuilder.InsertData("Test", new[] { "ID", "Description" }, new object[] { Guid.NewGuid(), "Test description 5" });
+            migrationBuilder.InsertData("Test", new[] { "ID", "Description" }, new object[] { Guid.NewGuid(), "Test description 6" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Test;");
+        }
+    }
+}
